aab76a52bc9a354c90da781a259779b0935f1c95
Jonatas Oliveira Neto <jonatas.oliveiraneto@everis.nttdata.com>
2023-04-13 19:18:56 +0100

    #22222 - Pedido do chefe

--- src/Demos/ProjetoSQL/Moongy.Biblioteca.Database/Moongy.Biblioteca.Database/dbo/vwBibliotecaLivros.sql
+++ src/Demos/ProjetoSQL/Moongy.Biblioteca.Database/Moongy.Biblioteca.Database/dbo/vwBibliotecaLivros.sql
@@ -1,5 +1,5 @@
 ﻿CREATE VIEW [dbo].[vwBibliotecaLivros]
 AS 
-	SELECT l.Nome AS NomeLivro, b.Nome AS NomeBiblioteca
+	SELECT l.Nome AS Livro, b.Nome AS Biblio
 	FROM Livros AS l
 	LEFT JOIN Biblioteca AS b ON l.BibliotecaId = b.Id
